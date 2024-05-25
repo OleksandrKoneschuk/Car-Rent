faadf913632e81c69123d7f1f7744bbb171594e4
Koneshchuk Oleksandr <ipz222_kos@student.ztu.edu.ua>
2024-05-26 02:58:55 +0300

    Refactored DataBase.cs: - Introduced abstract DataBase class. - Added SqlDataBase and MySqlDataBase classes with singleton pattern. - Implemented connection management methods.

--- VS/Course/MyLib/DataBase.cs
+++ VS/Course/MyLib/DataBase.cs
@@ -2,32 +2,93 @@
 
 namespace MyLib
 {
-    public class DataBase
+    public abstract class DataBase
     {
-        SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-FUAH6BA;Initial Catalog=Car_Rent;Integrated Security=True");
+        protected SqlConnection sqlConnection;
 
-
-        public void openConnection()
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+        public SqlConnection GetSqlConnection()
         {
-            if(sqlConnection.State == System.Data.ConnectionState.Closed)
+            return sqlConnection;
+        }
+    }
+
+    public class SqlDataBase : DataBase
+    {
+        private static SqlDataBase instance;
+
+        private SqlDataBase()
+        {
+            sqlConnection = new SqlConnection(@"Data Source=DESKTOP-FUAH6BA;Initial Catalog=Car_Rent;Integrated Security=True");
+        }
+
+        public static SqlDataBase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SqlDataBase();
+                }
+                return instance;
+            }
+        }
+
+        public override void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
             {
                 sqlConnection.Open();
             }
         }
 
-        public void closeConnection()
+        public override void CloseConnection()
         {
-            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
             {
                 sqlConnection.Close();
             }
         }
+    }
 
-        public SqlConnection getSqlConnection()
+    // Приклад іншого типу бази даних, наприклад, для MySQL
+    public class MySqlDataBase : DataBase
+    {
+        private static MySqlDataBase instance;
+
+        private MySqlDataBase()
         {
-            return sqlConnection;
+            // Ініціалізація з'єднання з MySQL базою даних
+            // sqlConnection = new MySqlConnection("your_connection_string_here");
         }
 
+        public static MySqlDataBase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MySqlDataBase();
+                }
+                return instance;
+            }
+        }
 
+        public override void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public override void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
     }
 }
