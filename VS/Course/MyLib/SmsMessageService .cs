76b416e6b92c647aed609ac80dfae795fe5b058d
Koneshchuk Oleksandr <ipz222_kos@student.ztu.edu.ua>
2024-05-26 03:05:18 +0300

    Added IMessageService interface and SmsMessageService implementation.

---
+++ VS/Course/MyLib/SmsMessageService .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class SmsMessageService : IMessageService
+    {
+        public void SendMessage(string phoneNumber, string message)
+        {
+            Console.WriteLine($"Sending SMS to {phoneNumber}: {message}");
+        }
+    }
+}
