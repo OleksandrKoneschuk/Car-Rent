ae441db32d610bcad14975f243b7ed0d777c2d2c
Koneshchuk Oleksandr <ipz222_kos@student.ztu.edu.ua>
2024-05-26 03:17:35 +0300

    Refactored MainWindow.xaml.cs: added observer pattern, improved database interaction, and enhanced car selection.

--- VS/Course/Course/MainWindow.xaml.cs
+++ VS/Course/Course/MainWindow.xaml.cs
@@ -5,31 +5,33 @@ using System.Collections.ObjectModel;
 using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Input;
 
 namespace Course
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IObserver
     {
-        private ObservableCollection<Car> cars;
-        DataBase dataBase = new DataBase();
+        private readonly ObservableCollection<Car> cars = new ObservableCollection<Car>();
+        private readonly CarDataNotifier carDataNotifier = new CarDataNotifier();
+        private readonly DataBase dataBase = SqlDataBase.Instance;
 
         private int _idKlient;
-
         private DateTime _startDate;
         private DateTime _endDate;
+
         public MainWindow(int idKlient)
         {
             InitializeComponent();
             _idKlient = idKlient;
-
-            cars = new ObservableCollection<Car>();
             carListBox.ItemsSource = cars;
+            carDataNotifier.Attach(this);
 
-
-
+            InitializeDates();
             UpdateCarsCollection("SELECT * FROM Avto");
+        }
 
+        private void InitializeDates()
+        {
             DateTime currentDate = DateTime.Now;
             DateTime nextDay = currentDate.AddDays(1);
             Start_Date_DatePicker.DisplayDateStart = currentDate;
@@ -39,8 +41,12 @@ namespace Course
             _endDate = nextDay;
         }
 
+        public void Update()
+        {
+            UpdateCarsCollection("SELECT * FROM Avto");
+        }
 
-        private void sortComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private void sortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBoxItem selectedItem = (ComboBoxItem)sortComboBox.SelectedItem;
             if (selectedItem != null)
@@ -51,17 +57,15 @@ namespace Course
             }
         }
 
-
-        private void Search()
-        {
-            string queryString = $"SELECT * FROM Avto WHERE concat (ID_Avto, car_Number, car_Brand, car_Model, car_Color, car_Year, average_Fuel_Consumption) like '%" + searchTextBox.Text + "%'";
-            UpdateCarsCollection(queryString);
-        }
-
-
         private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             Search();
+        }
+
+        private void Search()
+        {
+            string queryString = $"SELECT * FROM Avto WHERE CONCAT(ID_Avto, car_Number, car_Brand, car_Model, car_Color, car_Year, average_Fuel_Consumption) LIKE '%" + searchTextBox.Text + "%'";
+            UpdateCarsCollection(queryString);
         }
 
         private void UpdateCarsCollection(string queryString)
@@ -73,16 +77,47 @@ namespace Course
             }
         }
 
+        private List<Car> GetCarData(string queryString)
+        {
+            List<Car> carList = new List<Car>();
+            dataBase.OpenConnection();
+
+            using (SqlCommand command = new SqlCommand(queryString, dataBase.GetSqlConnection()))
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var car = new Car
+                    {
+                        ID_Avto = (int)reader["ID_Avto"],
+                        CarNumber = reader["car_Number"].ToString(),
+                        CarBrand = reader["car_Brand"].ToString(),
+                        CarModel = reader["car_Model"].ToString(),
+                        CarColor = reader["car_Color"].ToString(),
+                        CarYear = (int)reader["car_Year"],
+                        AverageFuelConsumption = (decimal)reader["average_Fuel_Consumption"],
+                        PhotoData = reader["PhotoData"] as byte[],
+                        Cost1_3DayRental = (decimal)reader["cost_1_3_Day_Rental"],
+                        Cost4_9DayRental = (decimal)reader["cost_4_9_Day_Rental"],
+                        Cost10_25DayRental = (decimal)reader["cost_10_25_Day_Rental"],
+                        Cost26DayRental = (decimal)reader["cost_26_Day_Rental"]
+                    };
+                    carList.Add(car);
+                }
+            }
+
+            dataBase.CloseConnection();
+            return carList;
+        }
+
         private void button_FindAvto_Click(object sender, RoutedEventArgs e)
         {
             _startDate = (Start_Date_DatePicker.SelectedDate ?? DateTime.MinValue).Date;
-            var startDate = _startDate.ToString("yyyy-MM-dd");
             _endDate = (end_Of_Rental_DatePicker.SelectedDate ?? DateTime.MinValue).Date;
-            var endDate = _endDate.ToString("yyyy-MM-dd");
 
             if (_startDate < _endDate)
             {
-                string queryString = $"SELECT Avto.* FROM Avto LEFT JOIN Rent ON Avto.ID_Avto = Rent.ID_Avto WHERE Rent.ID_Renta IS NULL OR (Rent.end_Of_Rental < '{startDate}' OR Rent.rental_Start_Date > '{endDate}');";
+                string queryString = $"SELECT Avto.* FROM Avto LEFT JOIN Rent ON Avto.ID_Avto = Rent.ID_Avto WHERE Rent.ID_Renta IS NULL OR (Rent.end_Of_Rental < '{_startDate:yyyy-MM-dd}' OR Rent.rental_Start_Date > '{_endDate:yyyy-MM-dd}')";
                 UpdateCarsCollection(queryString);
             }
             else
@@ -90,55 +125,6 @@ namespace Course
                 MessageBox.Show("Дата початку повинна бути менше за дату завершення.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
-        private List<Car> GetCarData(string queryString)
-        {
-            try
-            {
-                dataBase.openConnection();
-
-                using (SqlCommand command = new SqlCommand(queryString, dataBase.getSqlConnection()))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        List<Car> cars = new List<Car>();
-
-                        while (reader.Read())
-                        {
-                            string carBrand = reader["car_Brand"].ToString();
-                            string carModel = reader["car_Model"].ToString();
-                            Car car = new Car
-                            {
-                                ID_Avto = (int)reader["ID_Avto"],
-                                CarNumber = reader["car_Number"].ToString(),
-                                CarBrandModel = $"{carBrand} {carModel}",
-                                CarColor = reader["car_Color"].ToString(),
-                                CarYear = (int)reader["car_Year"],
-                                AverageFuelConsumption = (decimal)reader["average_Fuel_Consumption"],
-                                PhotoData = reader["PhotoData"] as byte[],
-                                Cost1_3DayRental = (decimal)reader["cost_1_3_Day_Rental"],
-                                Cost4_9DayRental = (decimal)reader["cost_4_9_Day_Rental"],
-                                Cost10_25DayRental = (decimal)reader["cost_10_25_Day_Rental"],
-                                Cost26DayRental = (decimal)reader["cost_26_Day_Rental"]
-                            };
-
-                            cars.Add(car);
-                        }
-                        return cars;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Помилка при отриманні даних про авто: {ex.Message}", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return null;
-            }
-            finally
-            {
-                dataBase.closeConnection();
-            }
-        }
-
 
         private void Profile_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -149,14 +135,13 @@ namespace Course
 
         private void button_ChooseCar_Click(object sender, RoutedEventArgs e)
         {
-            Button clickedButton = sender as Button;
-            if (clickedButton != null)
+            if (sender is Button clickedButton)
             {
                 if (_startDate < _endDate)
                 {
                     int idAvto = (int)clickedButton.Tag;
-
-                    ChooseCarWindow childWindow = new ChooseCarWindow(_startDate, _endDate, idAvto, _idKlient);
+                    IMessageService messageService = new SmsMessageService();
+                    ChooseCarWindow childWindow = new ChooseCarWindow(_startDate, _endDate, idAvto, _idKlient, messageService);
                     childWindow.Owner = this;
                     childWindow.ShowDialog();
                 }
@@ -165,6 +150,11 @@ namespace Course
                     MessageBox.Show("Дата початку повинна бути менше за дату завершення.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            carDataNotifier.Detach(this);
         }
     }
 }
